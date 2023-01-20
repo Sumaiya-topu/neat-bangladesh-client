5030b7930d187256d3a2e5dd9fd93fb401738f6b
sanchitadevi <thesanchitadevi@gmail.com>
2023-01-20 19:41:38 +0600

    first commit

---
+++ src/Routes/routes.js
@@ -0,0 +1,15 @@
+const { createBrowserRouter } = require("react-router-dom");
+const { default: Main } = require("../Layout/Main");
+
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main></Main>,
+        // children: [
+        //     {
+        //         path: "/",
+        //         element:
+        //     }
+        // ]
+    }
+])
